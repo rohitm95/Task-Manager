e3d2c0ca4ea5ce7494d2668e218b06e7d2c1ed71
Rohit Mamidwar <rohitm95@gmail.com>
2024-10-06 14:04:34 +0530

    Removed unused imports

--- src/app/todos/list/list.component.ts
+++ src/app/todos/list/list.component.ts
@@ -19,7 +19,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatPaginator } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -73,7 +72,6 @@ export class ListComponent implements AfterViewInit, OnInit, OnDestroy {
   authState$ = authState(this.auth);
   snackbarService = inject(SnackbarService);
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor() {}
